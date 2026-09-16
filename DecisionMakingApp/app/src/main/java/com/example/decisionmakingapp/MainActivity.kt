38c77696f993c6962bebf4d0921d2cbb421eeeb4
Hiral <101991274+hiralh@users.noreply.github.com>
2026-09-15 20:07:38 -0600

    Adding some edits to UI.

--- DecisionMakingApp/app/src/main/java/com/example/decisionmakingapp/MainActivity.kt
+++ DecisionMakingApp/app/src/main/java/com/example/decisionmakingapp/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -20,11 +24,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.decisionmakingapp.ui.theme.DecisionMakingAppTheme
+import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -79,24 +90,34 @@ fun Screen(modifier: Modifier,
             contentDescription = null,
             contentScale = ContentScale.Crop
         )
-        //UI for message and buttons
-        Box(modifier = Modifier.size(width = 500.dp, height = 500.dp), contentAlignment = Alignment.Center) {
+        //UI for messages and buttons
+        Column(modifier = Modifier.size(width = 500.dp, height = 500.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
             // For message
             Row(modifier = Modifier.padding(8.dp))
             {
                 TextDisplay(modifier, message)
             }
+            Spacer(modifier = Modifier.height(10.dp))
             // For buttons
             Row() {
-                Button(onClick = {yesButton()}){
+                Button(onClick = {yesButton()},
+                    shape  = RoundedCornerShape(12.dp),
+                    modifier = Modifier
+                        .height((50.dp))){
                     Text("YES!")
                 }
-                Spacer(modifier = Modifier.width(8.dp))
-                Button(onClick = {maybeButton()}){
+                Spacer(modifier = Modifier.width(10.dp))
+                Button(onClick = {maybeButton()},
+                    shape  = RoundedCornerShape(12.dp),
+                    modifier = Modifier
+                        .height((50.dp))){
                     Text("Maybe...")
                 }
-                Spacer(modifier = Modifier.width(8.dp))
-                Button(onClick = {noButton}){
+                Spacer(modifier = Modifier.width(10.dp))
+                Button(onClick = {noButton},
+                    shape  = RoundedCornerShape(12.dp),
+                    modifier = Modifier
+                        .height((50.dp))){
                     Text("Nope!")
                 }
             }
@@ -106,7 +127,16 @@ fun Screen(modifier: Modifier,
 
 @Composable
 fun TextDisplay(modifier: Modifier, message: String){
-    // Text details
+    Text(
+        message,
+        modifier = modifier,
+        color = Color.Black,
+        fontSize = 50.sp,
+        textAlign = TextAlign.Center,
+        //lineHeight = 70.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.SansSerif
+    )
 }
 
 @Preview(showBackground = true)
